e962f216a99ff1666f4dd485a36c678788a17674
gdeeeeyy <gd14405@gmail.com>
2023-11-01 00:22:16 +0530

    logistic regression works lesgooo

--- asd.py
+++ asd.py
@@ -1,7 +1,8 @@
 import numpy as np
-import matplotlib.pyplot as ply
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 csv=pd.read_csv(r"asd.csv")
 data=pd.DataFrame(csv)
@@ -25,16 +26,27 @@ for i,j in fdat.items():
 
 data=data.fillna(0)
 data=data.drop('Ethnicity', axis=1)
-rep={'Yes':1, 'No':0, 'yes':1, 'no':0, 'f':1, 'm':0}
+rep={'Yes':1, 'No': 0, 'yes':1, 'no':0, 'f':1, 'm':0}
 data=data.replace(rep)
 
 data.to_csv('finalasd.csv', index=False)
 
 csvf=pd.read_csv(r"finalasd.csv")
 df=pd.DataFrame(csvf)
-X=df.drop(['ASD', 'Who completed the test'], axis=1)
+X=df.drop(['Case_No', 'ASD', 'Who completed the test'], axis=1)
 y=df['ASD']
+
 X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.4, random_state=123)
+
+
+from sklearn.preprocessing import LabelEncoder
+label_encoder = LabelEncoder()
+
+for column in X_train.columns:
+    if X_train[column].dtype == 'object':
+        X_train[column] = label_encoder.fit_transform(X_train[column])
+        X_test[column] = label_encoder.transform(X_test[column])
+
 
 from knn import KNN
 clf=KNN(k=3)
@@ -43,3 +55,15 @@ preds=clf.predict(X_test)
 
 acc=np.sum(preds==y_test)/len(y_test)
 print(acc)
+
+
+def accuracy(y_true, y_pred):
+    acc=np.sum(y_true==y_pred)/len(y_true)
+    return acc
+
+from logreg import LogisticRegression
+reg=LogisticRegression(lr=0.0001, n_iters=1000000000)#run these iterations(didn't run this yet)
+reg.fit(X_train, y_train)
+preds=reg.predict(X_test)
+
+print("Accuracy for 1000000000 iterations = ",accuracy(y_test, preds)*100)
